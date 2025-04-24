317d250e51d8b221bd7594d1bff6fb312da01ebf
ampourmand <pourmand@terpmail.umd.edu>
2025-04-24 01:12:11 -0400

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by neo on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeditationTests/MeditationTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationTests.swift
+//  MeditationTests
+//
+//  Created by neo on 4/24/25.
+//
+
+import Testing
+@testable import Meditation
+
+struct MeditationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
